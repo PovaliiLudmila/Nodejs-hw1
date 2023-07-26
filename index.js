37d95132d3ddde0c1d0e6140733edf02f30a8aae
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-07-27 00:50:08 +0300

    1

---
+++ index.js
@@ -0,0 +1,29 @@
+const {program} = require("commander");
+
+const contacts = require("./contacts");
+const argv = require('yargs').argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      // ...
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
