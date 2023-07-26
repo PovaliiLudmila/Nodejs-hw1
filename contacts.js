e7a415671f7b3778710993c873976e234bfac019
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-07-27 01:28:20 +0300

    1

--- contacts.js
+++ contacts.js
@@ -1,36 +1,38 @@
-const fs = require('fs/promises');
+const fs = require('node:fs/promises');
 const path = require("path");
 const {nanoid} = require("nanoid");
  
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const listContacts = async() => {
+async function listContacts() {
     const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
   }
   
-const getContactById(contactId) = async (id) => {
+async function getContactById(id) {
         const contacts = await listContacts();
         const result = contacts.find(item => item.id === id);
         return result || null;
   }
   
-const removeContact(contactId) = async(id) => {
+async function removeContact(id) {
     const contacts = await listContacts();
     const index = contacts.findIndex(item => item.id === id);
     if(index === -1){
         return null;
     }
     const [result] = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(books, null, 2));
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return result;
 }
 
-const addContact(name, email, phone) = async(data) => {
+async function addContact(name, email, phone)  {
     const contacts = await listContacts();
     const newContact = {
         id: nanoid(),
-        ...data,
+        name: name,
+        email: email,
+        phone: phone,
     }
     contacts.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const {program} = require("commander");
+const { Command } = require("commander");
 
 const contacts = require("./contacts");
-const argv = require('yargs').argv;
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // ...
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case 'get':
-      // ... id
+      const oneContact = await contacts.getContactById(id);
+      console.table(oneContact);
       break;
 
     case 'add':
-      // ... name email phone
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
       break;
 
     case 'remove':
-      // ... id
+      const removeContactById = await contacts.removeContact(id);
+      console.table(removeContactById);
       break;
 
     default:
